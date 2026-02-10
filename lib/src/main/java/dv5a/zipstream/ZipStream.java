c3b548625b09c13a11e6dbbee9e0b209b9823402
Daniel Vega <daniel.mario.vega@gmail.com>
2026-02-10 17:15:13 -0300

    Add javadoc

--- lib/src/main/java/dv5a/zipstream/ZipStream.java
+++ lib/src/main/java/dv5a/zipstream/ZipStream.java
@@ -16,9 +16,20 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * A class for reading ZIP files.
+ * This class provides functionality to open a ZIP file, iterate over its entries,
+ * and access the raw data of each entry.
+ */
 public class ZipStream implements Closeable  {
     private final RandomAccessFile in;
 
+    /**
+     * Constructs a new ZipStream from the specified file.
+     *
+     * @param file the ZIP file to be opened.
+     * @throws IOException if an I/O error occurs while opening the file.
+     */
     public ZipStream(File file) throws IOException {
         this.in = new RandomAccessFile(file, "r");
         try {
@@ -30,6 +41,12 @@ public class ZipStream implements Closeable  {
         }
     }
 
+    /**
+     * Closes this stream and releases any system resources associated with it.
+     * If the stream is already closed, then invoking this method has no effect.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
     @Override
     public void close() throws IOException {
         in.close();
@@ -47,6 +64,9 @@ public class ZipStream implements Closeable  {
         }
     }
 
+    /**
+     * Represents an entry in a ZIP file.
+     */
     public static class Entry {
         final RandomAccessFile in;
         final int thisDiskNumber;
@@ -63,21 +83,48 @@ public class ZipStream implements Closeable  {
         final char extraFieldLength;
         final char commentLength;
 
+        /**
+         * Returns the compressed size of the entry data.
+         *
+         * @return the compressed size.
+         */
         public long getCompressedSize() {
             return compressedSize;
         }
 
+        /**
+         * Returns the uncompressed size of the entry data.
+         *
+         * @return the uncompressed size.
+         */
         public long getUncompressedSize() {
             return uncompressedSize;
         }
 
+        /**
+         * Returns the CRC-32 checksum of the uncompressed entry data.
+         *
+         * @return the CRC-32 checksum.
+         */
         public int getCrc32() {
             return crc32;
         }
 
+        /**
+         * Enumeration of compression methods.
+         */
         public enum Method {
+            /**
+             * The entry is stored (no compression).
+             */
             STORED,
+            /**
+             * The entry is deflated.
+             */
             DEFLATED,
+            /**
+             * The entry uses another compression method.
+             */
             OTHER
         }
 
@@ -107,14 +154,29 @@ public class ZipStream implements Closeable  {
             this.name = new String(nameBuffer.array(), charset);
         }
 
+        /**
+         * Returns the name of the entry.
+         *
+         * @return the entry name.
+         */
         public String getName() {
             return name;
         }
 
+        /**
+         * Returns the compression method used for this entry.
+         *
+         * @return the compression method as an integer.
+         */
         public int getCompressionMethod() {
             return method;
         }
 
+        /**
+         * Returns the compression method used for this entry as a {@link Method} enum.
+         *
+         * @return the compression method.
+         */
         public Method getMethod() {
             switch (method) {
                 case 0: return Method.STORED;
@@ -123,6 +185,13 @@ public class ZipStream implements Closeable  {
             }
         }
 
+        /**
+         * Returns an InputStream for reading the raw (compressed) data of this entry.
+         *
+         * @return an InputStream for the raw data.
+         * @throws IOException if an I/O error occurs.
+         * @throws ZipStreamException if the ZIP file structure is invalid or multi-disk ZIPs are encountered.
+         */
         public InputStream rawData() throws IOException {
             if (this.diskNumber != this.thisDiskNumber) {
                 throw new ZipStreamException("Multi-disk ZIP files not supported");
@@ -160,7 +229,7 @@ public class ZipStream implements Closeable  {
                 }
 
                 @Override
-                public void close() throws IOException {
+                public void close() {
                     pos = -1;
                 }
             };
@@ -176,6 +245,12 @@ public class ZipStream implements Closeable  {
         }
     }
 
+    /**
+     * Returns a collection of all entries in the ZIP file.
+     *
+     * @return a collection of {@link Entry} objects.
+     * @throws IOException if an I/O error occurs while reading the ZIP file structure.
+     */
     public Collection<Entry> entries() throws IOException {
         var eocd = findEOCD();
 
@@ -235,7 +310,7 @@ public class ZipStream implements Closeable  {
         var buff = ByteBuffer.allocate(BUFF_SIZE + 21).order(ByteOrder.LITTLE_ENDIAN);
         var fpos = size;
 
-        while (fpos > 0) {
+        while (true) {
             var nbytes = BUFF_SIZE;
             fpos -= BUFF_SIZE;
             if (fpos < 0) {
@@ -255,6 +330,7 @@ public class ZipStream implements Closeable  {
                     return new EOCD(diskNumber, entryCount, centralDirectoryOffset);
                 }
             }
+            if (fpos <= 0) break;
             var array = buff.array();
             System.arraycopy(array, 0, array, BUFF_SIZE, 21);
         }
